fe1b685f8fec65a2bea7c6e12a83721ae15859e1
Leomax43 <130073563+Leomax43@users.noreply.github.com>
2025-05-22 18:47:01 -0400

    brilloySonidoSemantienen

--- Scripts/menu_principal.gd
+++ Scripts/menu_principal.gd
@@ -1,6 +1,15 @@
 extends Control
+@onready var fondo_brillo = $ColorRect  # Ajusta la ruta si tu nodo tiene otro nombre o está en otra rama
 
 
+func aplicar_brillo(valor: float) -> void:
+	var color = fondo_brillo.color
+	color.a = 1- valor  # Valor entre 0 (claro) y 1 (oscuro)
+	fondo_brillo.color = color
+	
+func _ready():
+	var brillo_guardado = GlobalConfig.brillo  # O cualquier variable donde guardes el valor
+	aplicar_brillo(brillo_guardado)
 
 
 func _on_jugar_pressed() -> void:
--- Scripts/opciones.gd
+++ Scripts/opciones.gd
@@ -1,29 +1,52 @@
 extends Node2D
 
-
 @onready var slider = $Slider
-@onready var Opciones = $"."  # Ajusta esta ruta al nodo que quieres atenuar
+@onready var Opciones = $"."  # Ajusta si es necesario
 
+var config_path := "user://config.cfg"
+var config := ConfigFile.new()
 
 func _ready():
-	$VBoxContainer3/Sonido.value = db_to_linear(AudioServer.get_bus_volume_db(0))
-	
+	$VBoxContainer3/Sonido.value = GlobalConfig.volumen
+	$VBoxContainer4/Brillo.value = GlobalConfig.brillo
+	GlobalConfig.aplicar_config(Opciones)
 
+func aplicar_config(volumen: float, brillo: float) -> void:
+	AudioServer.set_bus_volume_db(0, linear_to_db(volumen))
+	var color = Opciones.modulate
+	color.a = brillo
+	Opciones.modulate = color
 
+func guardar_config():
+	config.set_value("audio", "volumen", $VBoxContainer3/Sonido.value)
+	config.set_value("pantalla", "brillo", $VBoxContainer4/Brillo.value)
+	var error = config.save(config_path)
+	if error != OK:
+		print("Error guardando configuración:", error)
+	else:
+		print("Configuración guardada")
+
+func cargar_config():
+	var error = config.load(config_path)
+	if error == OK:
+		var volumen = config.get_value("audio", "volumen", 1.0)
+		var brillo = config.get_value("pantalla", "brillo", 1.0)
+		$VBoxContainer3/Sonido.value = volumen
+		$VBoxContainer4/Brillo.value = brillo
+		aplicar_config(volumen, brillo)
+		print("Configuración cargada")
+	else:
+		print("No se encontró configuración previa")
+
+func _on_confirmar_pressed() -> void:
+	GlobalConfig.volumen = $VBoxContainer3/Sonido.value
+	GlobalConfig.brillo = $VBoxContainer4/Brillo.value
+	GlobalConfig.guardar_config()
+	GlobalConfig.aplicar_config(Opciones)
 
 func _on_volver_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/menuPrincipal.tscn")
 
-
-func _on_confirmar_pressed() -> void:
-	AudioServer.set_bus_volume_db(0, linear_to_db($VBoxContainer3/Sonido.value))
-	
-	var color = Opciones.modulate
-	color.a = $VBoxContainer4/Brillo.value  # Modificamos solo la opacidad (alfa)
-	Opciones.modulate = color
-	pass # Replace with function body.
-	
-	
 	
 
 
