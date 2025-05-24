1b90a56c8bd8596ddc1e2bbdec0f5f2c655ae9f4
sofipipi <sofia.contreras02@alumnos.ucn.cl>
2025-05-24 17:32:19 -0400

    aa
    aaa

--- Scripts/juego.gd
+++ Scripts/juego.gd
@@ -1,5 +1,10 @@
 extends Node2D
+@onready var jugador= get_node("Jugador")
+@onready var enemigo= get_node("Enemigo")
 
+func daño_al_jugador()->void:
+	if enemigo.is_on_group(jugador):
+		jugador.take_damage(300)
 
 func _on_volver_pressed() -> void:
 	get_tree().change_scene_to_file("res://menuPrincipal.tscn")
