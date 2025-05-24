7dd4150c569ebe23c1ce0a92c1dc3f3437643183
midnighght <msarnojimenez+github@gmail.com>
2025-05-24 16:28:58 -0400

    moon detection, player rework, main title update, signal update, button style update

--- Scripts/menu_principal.gd
+++ Scripts/menu_principal.gd
@@ -9,21 +9,5 @@ func _on_multiplayer_pressed() -> void:
 func _on_config_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/opciones.tscn")
 
-<<<<<<< Updated upstream
-func _on_jugar_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/juego.tscn")
-
-
-func _on_asincronico_pressed() -> void:
-	pass # Replace with function body.
-
-
-func _on_configuracion_pressed() -> void:
-	get_tree().change_scene_to_file("res://Prefabs/UI/opciones.tscn")
-
-
-func _on_salir_pressed() -> void:
-=======
 func _on_quit_pressed() -> void:
->>>>>>> Stashed changes
 	get_tree().quit()
