d7b0ab75211cb104f03081db8564cdca19b30959
sofipipi <sofia.contreras02@alumnos.ucn.cl>
2025-05-29 18:07:58 -0400

    Luz de la luna segun timer
    ahora esta conectado con la luz

--- Prefabs/Moon/moon.gd
+++ Prefabs/Moon/moon.gd
@@ -1,1 +1,16 @@
 extends Node2D
+var estado: bool
+@onready var tiempo= $Timer
+@onready var luz = $"Light Dynamics"
+func _ready():
+	luz.visible = false
+	tiempo.start(20)
+	tiempo.timeout.connect(_on_timer_timeout)
+
+
+##funcion para señal de lo que pase una vez que 
+##ya haya pasado el tiempo
+func _on_timer_timeout():
+	luz.visible = true
+	print("LUZ")
+	
