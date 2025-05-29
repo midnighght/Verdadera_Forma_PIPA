56c758d2a8d81a7e685fc4e408c7e9bc8f9de08c
sofipipi <sofia.contreras02@alumnos.ucn.cl>
2025-05-29 18:56:24 -0400

    Luna con luz y sombras
    esta intercalado con  la luna como encendida por ciertos segundos y la otra forma por otros segundos

--- Prefabs/Moon/moon.gd
+++ Prefabs/Moon/moon.gd
@@ -5,13 +5,23 @@ extends Node2D
 var encendida = false
 
 func _ready():
+	randomize()
 	luz.visible = false
-	tiempo.wait_time = 20
-	tiempo.one_shot = false  # se repetirá automáticamente
 	tiempo.timeout.connect(_on_timer_timeout)
-	tiempo.start()
+	esperar_proximo_cambio()
 
 func _on_timer_timeout():
-	encendida = !encendida         # alterna el estado
+	encendida = !encendida
 	luz.visible = encendida
-	print("LUZ:", encendida)
+	print("LUZ encendida:", encendida)
+	esperar_proximo_cambio()
+
+func esperar_proximo_cambio():
+	if encendida:
+		tiempo.wait_time = randf_range(10.0, 20.0)  # tiempo prendida
+		print("luz con sombras por ",tiempo.wait_time, "segundos")
+	else:
+		tiempo.wait_time = randf_range(3.0, 6.0) 
+		print("luz prendida por ",tiempo.wait_time, "segundos")   # tiempo apagada
+	tiempo.start()
+	
