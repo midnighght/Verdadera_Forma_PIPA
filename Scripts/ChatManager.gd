9fcb3cda5c49cdcf3a61bd86af9658be79b78112
sofipipi <sofia.contreras02@alumnos.ucn.cl>
2025-07-05 13:16:43 -0400

    ARCHIVOS SUBIDOS

---
+++ Scripts/ChatManager.gd
@@ -0,0 +1,122 @@
+extends Control
+
+# URL de conexión
+var _host = "ws://ucn-game-server.martux.cl/ws"
+@onready var _client: WebSocketClient = $WebSocketClient
+
+# Referencias a los nodos de la UI
+@onready var chat_display: TextEdit = $VBoxContainer/MainPanel/ChatDisplay
+@onready var player_list: ItemList = $VBoxContainer/MainPanel/UserList
+
+@onready var input_message: LineEdit = $VBoxContainer/Commands/InputMessage
+@onready var send_button: Button = $VBoxContainer/Commands/SendButton
+
+
+
+# Señales
+
+
+# Cuando se cierra la conexión
+func _on_web_socket_client_connection_closed():
+	var ws = _client.get_socket()
+	_sendToChatDisplay("Client just disconnected with code: %s, reason: %s" % [ws.get_close_code(), ws.get_close_reason()])
+
+# Cuando se conecta al servidor
+func _on_web_socket_client_connected_to_server():
+	var login_event = {
+		"event": "login",
+		"data": { "gameKey": "UZ78ZY" }
+	}
+	_client.send(JSON.stringify(login_event))
+
+# Gestor de mensajes del servidor
+func _on_web_socket_client_message_received(message: String):
+	var response = JSON.parse_string(message)
+	
+	match(response.event):
+		"connected-to-server":
+			_sendToChatDisplay("You are connected to the server!")
+		"public-message":
+			_sendToChatDisplay("%s: %s" % [response.data.id, response.data.msg])
+		"get-connected-players":
+			_updateUserList(response.data)
+		"player-connected":
+			_addUserToList(response.data.id)
+		"player-disconnected":
+			_deleteUserFromList(response.data.id)
+
+# Señales de la UI
+# Cuando se envia un mensaje desde el input
+func _on_input_submitted(message:String): 
+	if input_message.text == "":
+		return
+		
+	_sendMessage(message)
+	input_message.text = ""
+
+# Cuando se presiona el botón de "SEND"
+func _on_send_pressed():
+	if input_message.text == "":
+		return
+
+	_sendMessage(input_message.text)
+	input_message.text = ""
+
+# Cuando se pulsa el botón de "CONNECT TO SERVER"
+func _on_connect_toggled(pressed):
+	if not pressed:
+		_client.close()
+		return
+	_sendToChatDisplay("Connecting to host: %s." % [_host])
+	var err = _client.connect_to_url(_host)
+	if err != OK:
+		_sendToChatDisplay("Error connecting to host: %s" % [_host])
+		return
+
+# Funciones de la clase
+# Agrega un mensaje en la pantalla de chat
+func _sendToChatDisplay(msg):
+	print(msg)
+	chat_display.text += str(msg) + "\n"
+
+# Envía un mensaje a un usuario o al chat grupal y manda la solicitud al servidor
+func _sendMessage(message: String, userId: String = ''):
+	var action
+	if (userId != ''):
+		action = 'send-private-message'
+	else:
+		action = 'send-public-message'
+		
+	var dataToSend = {
+		"event": action,
+		"data": {
+			"message": message
+		}
+	}
+
+	_client.send(JSON.stringify(dataToSend))
+
+# Solicita la lista de usuarios activos al servidor
+func _sendGetUserListEvent():
+	var dataToSend = {
+		"event": 'get-connected-players'
+	}
+	_client.send(JSON.stringify(dataToSend))
+
+# Actualiza la lista de usuarios de la interfaz gráfica
+func _updateUserList(users: Array):
+	player_list.clear()
+	for user in users:
+		player_list.add_item(user)
+
+# Agrega un jugador al listado
+func _addUserToList(user: String):
+	player_list.add_item(user)
+
+# Elimina un jugador del listado
+func _deleteUserFromList(userId: String):
+	var playerListCount = player_list.item_count
+	for i in range(0, playerListCount):
+		if(player_list.get_item_text(i) == userId):
+			player_list.remove_item(i)
+			return
